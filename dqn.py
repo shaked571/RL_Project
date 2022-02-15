1a8038840c78e960038a1018ef7f79ad7c139321
Shaked Greenfeld <shakedg@aspectiva.com>
2022-02-15 17:29:21 +0200

    dqn

--- dqn.py
+++ dqn.py
@@ -1,121 +1,51 @@
+import itertools
+
 import gym
 import numpy as np
 import torch
 from algorithm import Algo
+from buffer import ReplayBuffer
 from models import DQNModel
 
 
 class DQN(Algo):
 
-    def __init__(self, gamma, epsilon, lr, batch_size, env, max_mem_size=100000, eps_end=0.05,
+    def __init__(self,   env, action_space,buffer, device,batch_size=64, lr=0.001, max_mem_size=100000, epsilon=1, eps_end=0.01,
                  eps_dec=5e-4):
-        super().__init__(env)
-        self.gamma = gamma
+        super().__init__(env, 'dqn')
         self.epsilon = epsilon
         self.eps_min = eps_end
         self.eps_dec = eps_dec
         self.lr = lr
-        self.mem_size = max_mem_size
-        self.batch_size = batch_size
-        self.mem_cntr = 0
-        self.iter_cntr = 0
-        self.replace_target = 100
+        self.action_space = action_space
+        self.num_actions = len(self.action_space)
+        self.device = device
+
         self.state_dim = env.observation_space.shape[0]
         self.action_dim = env.action_space.shape[0]
 
-        self.Q_eval = DQNModel(state_dim=self.state_dim, action_dim=self.action_dim, hidden_dim=256)
-        self.Q_next = DQNModel(state_dim=self.state_dim, action_dim=self.action_dim, hidden_dim=256)
+        self.Q_net = DQNModel(state_dim=self.state_dim, action_dim=self.action_dim, hidden_dim=256)
+        self.Q_target = DQNModel(state_dim=self.state_dim, action_dim=self.action_dim, hidden_dim=256)
 
-        self.state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
-        self.new_state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
-        self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
-        self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
-        self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool)
+    def run_algo_step(self, i):
 
-    def store_transition(self, state, action, reward, state_, terminal):
-        index = self.mem_cntr % self.mem_size
-        self.state_memory[index] = state
-        self.new_state_memory[index] = state_
-        self.reward_memory[index] = reward
-        self.action_memory[index] = action
-        self.terminal_memory[index] = terminal
+def main():
+    seed = 2022
 
-        self.mem_cntr += 1
+    # action_space = generate_action_spaces([0,0.3, 0.6, 0.9], [0, 0.6, -0.6, 0.9, -0.9])
+    action_space = {i: v for i, v in enumerate(itertools.product(*[np.linspace(-0.8, 0.8, 5)] * 4))}
 
-    def choose_action(self, observation):
-        if np.random.random() > self.epsilon:
-            state = torch.tensor([observation]).float().to(self.Q_eval.device)
-            actions = self.Q_eval(state)
-            action = torch.argmax(actions).item()
-        else:
-            action = np.random.choice(self.action_space)
+    env = gym.make("LunarLanderContinuous-v2")
+    env.action_space.np_random.seed(seed)
+    # random.seed(seed)
+    torch.manual_seed(seed)
+    rng = np.random.default_rng(seed)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    MAX_REPLAY_BUFFER = 100000
+    buffer = ReplayBuffer(MAX_REPLAY_BUFFER, device, rng)
 
-        return action
-
-    def learn(self):
-        if self.mem_cntr < self.batch_size:
-            return
-
-        self.Q_eval.optimizer.zero_grad()
-
-        max_mem = min(self.mem_cntr, self.mem_size)
-
-        batch = np.random.choice(max_mem, self.batch_size, replace=False)  # no duplicates indexes
-
-        batch_index = np.arange(self.batch_size, dtype=np.int32)
-
-        state_batch = torch.tensor(self.state_memory[batch]).to(self.Q_eval.device)
-        new_state_batch = torch.tensor(self.new_state_memory[batch]).to(self.Q_eval.device)
-        action_batch = self.action_memory[batch]
-        reward_batch = torch.tensor(self.reward_memory[batch]).to(self.Q_eval.device)
-        terminal_batch = torch.tensor(self.terminal_memory[batch]).to(self.Q_eval.device)
-
-        # get the q value of every sample of the action played
-        q_eval = self.Q_eval(state_batch)[batch_index, action_batch]
-        q_next = self.Q_eval(new_state_batch)
-        q_next[terminal_batch] = 0.0
-
-        q_target = reward_batch + self.gamma * torch.max(q_next, dim=1)[0]  # take max q value for every sample
-
-        loss = self.Q_eval.loss(q_target, q_eval).to(self.Q_eval.device)
-        loss.backward()
-        self.Q_eval.optimizer.step()
-
-        self.iter_cntr += 1
-        self.epsilon = self.epsilon - self.eps_dec if self.epsilon > self.eps_min else self.eps_min
-
+    algo = DQN(env,action_space, buffer, device)
+    algo.run_all_episodes()
 
 if __name__ == '__main__':
-    env = gym.make("BipedalWalker-v3")
-    input_dims = len(env.reset())  # how many elements does the state representation have?
-    agent = DQN(env=env, gamma=0.99, epsilon=1.0, batch_size=4, eps_end=0.01,
-                     lr=0.003)
-    scores, avg_scores, eps_history = [], [], []
-
-
-    #TODO -refactor!!!
-    epochs = 500
-
-    for epoch in range(epochs):
-        score = 0
-        done = False
-        state_old = env.reset()
-        # print(state_old[0].type)
-        while not done:  # iterating over every timestep (state)
-            env.render()
-            action = agent.choose_action(state_old)
-            state_new, reward, done, info = env.step(action)
-            score += reward
-
-            agent.store_transition(state_old, action, reward, state_new, done)
-            agent.learn()
-            state_old = state_new
-
-        scores.append(score)
-        eps_history.append(agent.epsilon)
-        avg_score = np.mean(scores[-100:])
-        avg_scores.append(avg_score)
-
-        print("epoch: ", epoch, "score: %.2f " % score, "avg_score: %.2f " % avg_score, "epsilon: %.2f" % agent.epsilon)
-
-    env.close()
+    main()
